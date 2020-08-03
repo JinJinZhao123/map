b9fb14814bd810e938400925a6ac03024dce227f
JinJinZhao123 <295318654@qq.com>
2020-08-03 17:16:53 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,9 @@
   </header>
   <section class="map_content">
     <div class="map_tab">
-      <div class="map_tab_top" :class="!flag?'checked':''">确诊病例</div>
-      <div class="map_tab_top" :class="flag?'checked':''">疑似病例</div>
-      <div class="map_tab_top" :class="flag?'checked':''">治愈病例</div>
+      <div class="map_tab_top" :class="flagCon?'checked':''" @click="confirmed()">确诊病例</div>
+      <div class="map_tab_top" :class="flagSus?'checked':''" @click="suspected()">死亡病例</div>
+      <div class="map_tab_top" :class="flagCur?'checked':''" @click="cured()">新增确诊</div>
     </div>
     <div class="map_main" ref="mapbox"></div>  
   </section>
@@ -18,147 +18,7 @@
 import echarts from 'echarts'
 import 'echarts/map/js/china.js'
 import jsonp from 'jsonp'
-window.dataList = [{
-        name: "南海诸岛",
-        value: 0
-    },
-    {
-        name: '北京',
-        value: 54
-    },
-    {
-        name: '天津',
-        value: 13
-    },
-    {
-        name: '上海',
-        value: 40
-    },
-    {
-        name: '重庆',
-        value: 75
-    },
-    {
-        name: '河北',
-        value: 13
-    },
-    {
-        name: '河南',
-        value: 83
-    },
-    {
-        name: '云南',
-        value: 11
-    },
-    {
-        name: '辽宁',
-        value: 19
-    },
-    {
-        name: '黑龙江',
-        value: 15
-    },
-    {
-        name: '湖南',
-        value: 69
-    },
-    {
-        name: '安徽',
-        value: 60
-    },
-    {
-        name: '山东',
-        value: 39
-    },
-    {
-        name: '新疆',
-        value: 4
-    },
-    {
-        name: '江苏',
-        value: 31
-    },
-    {
-        name: '浙江',
-        value: 104
-    },
-    {
-        name: '江西',
-        value: 36
-    },
-    {
-        name: '湖北',
-        value: 1052
-    },
-    {
-        name: '广西',
-        value: 33
-    },
-    {
-        name: '甘肃',
-        value: 7
-    },
-    {
-        name: '山西',
-        value: 9
-    },
-    {
-        name: '内蒙古',
-        value: 7
-    },
-    {
-        name: '陕西',
-        value: 22
-    },
-    {
-        name: '吉林',
-        value: 4
-    },
-    {
-        name: '福建',
-        value: 18
-    },
-    {
-        name: '贵州',
-        value: 5
-    },
-    {
-        name: '广东',
-        value: 98
-    },
-    {
-        name: '青海',
-        value: 1
-    },
-    {
-        name: '西藏',
-        value: 0
-    },
-    {
-        name: '四川',
-        value: 44
-    },
-    {
-        name: '宁夏',
-        value: 4
-    },
-    {
-        name: '海南',
-        value: 22
-    },
-    {
-        name: '台湾',
-        value: 3
-    },
-    {
-        name: '香港',
-        value: 5
-    },
-    {
-        name: '澳门',
-        value: 5
-    }
-];
+
 export default {
   name: 'HelloWorld',
   props: {//组件的属性
@@ -166,8 +26,12 @@ export default {
   },
   data () {
     return {
-      flag: false,
+    //   flag: false,
+      flagCon: true,
+      flagSus: false,
+      flagCur: false,
       time: new Date(),
+      timer: null, //定时器
       option : {
         tooltip: {
             triggerOn: "click",
@@ -183,29 +47,29 @@ export default {
             showLabel: !0,
             text: ["高", "低"],
             pieces: [{
-                gt: 10000,
+                gte: 10000,
                 label: "> 10000",
-                color: "#660208"
+                color: "#b80909"
             }, {
                 gte: 1000,
                 lte: 9999,
                 label: "1000 - 9999",
-                color: "#8C0D0D"
+                color: "#e64546"
             },{
                 gte: 100,
                 lte: 999,
                 label: "100 - 999",
-                color: "#CC2929"
+                color: "#f57567"
             }, {
                 gte: 10,
-                lt: 99,
+                lte: 99,
                 label: "10 - 99",
-                color: "#FF7B69"
+                color: "#ff9985"
             }, {
-                gt: 1,
-                lt: 9,
+                gte: 1,
+                lte: 9,
                 label: "1 - 9",
-                color: "#FFAA85"
+                color: "#ffe5db"
             }, {
                 value: 0,
                 color: "#ffffff"
@@ -247,45 +111,75 @@ export default {
             name: "确诊病例",
             type: "map",
             geoIndex: 0,
-            data: window.dataList
+            data: []
         }]
       }
     }
   },
   mounted () {
     this.getData()
-    let myChart = echarts.init(this.$refs.mapbox);
-    myChart.setOption(this.option);
+    this.myChart = echarts.init(this.$refs.mapbox);
+    this.timer = setInterval(() => {
+       setTimeout(this.getData, 0)
+    }, 1000*60*60)
   },
   methods:{
     getData () {
       jsonp('https://voice.baidu.com/newpneumonia/get?target=trend&isCaseIn=0&stage=publish&callback=jsonp_1596355876712_14463', {}, (err, data) => {
         if (!err) {
-          console.log(data)
-          var Y = this.time.getFullYear();
+        //   var Y = this.time.getFullYear();
           var M = this.time.getMonth() + 1;
-          var D = this.time.getDate();
-          var day = (new Date(Y,M,D).getTime()-new Date(2020,1,26).getTime())/(1000*60*60*24);
-          let arr = [];
-          // var data = data.data
+          var D = this.time.getDate() - 1;
+          var md = M + "." + D;  //与数据格式一致的当前日期
+          let arr = [],
+          finalData;
           for (var item of data.data) {
-            // console.log(day)
-            // console.log(item.trend.list[0].data[188])
-            console.log(item.trend.list[0].data.length)
+            var arr_date = item.trend.updateDate;
+            var index = arr_date.indexOf(md)!=-1?arr_date.indexOf(md):arr_date.length-1;//当前日期的索引
+            if(this.flagCon){
+                finalData = item.trend.list[0];
+                console.log(finalData)
+            }else if(this.flagSus) {
+                finalData = item.trend.list[2];
+                console.log(finalData)
+            }else if(this.flagCur) {
+                finalData = item.trend.list[3];
+                console.log(finalData)
+            }
             arr.push({
               name: item.name,
-              value: item.trend.list[0].data[day-1]
+              value: finalData.data[index]//当前日期对应的数据
             });
           }
-          console.log(arr)
-          // let list = data.data.map(item => ({name: item.name, value: item.value}))
-          // option.series[0].data = list
-          // this.mycharts.setOption(option)
+          this.option.series[0].data = arr;
+          this.myChart.setOption(this.option);
           // echarts初始化的前提是dom渲染完成
         }
       })
+    },
+    confirmed() {
+        this.flagCon = true;
+        this.flagSus = false;
+        this.flagCur = false;
+        this.getData();
+    },
+    suspected() {
+        this.flagCon = false;
+        this.flagSus = true;
+        this.flagCur = false;
+        this.getData();
+    },
+    cured() {
+        this.flagCon = false;
+        this.flagSus = false;
+        this.flagCur = true;
+        this.getData();
     }
-  }
+  },
+    beforeDestroy(){
+        clearInterval(this.timer);        
+        this.timer = null;
+    }
 }
 </script>
 
@@ -329,6 +223,7 @@ header h1{
   font-size: 16px;
   text-align: center;
   background-color: #ddd;
+  cursor:pointer;
 }
 .map_main{
   flex: 1;
